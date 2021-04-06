5a38a8dd743d25c3fcbb60d7f3786ce490faa77d
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-04-06 16:00:54 -0700

    Working on adding mouseover to nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+
+// SELECTORS //
+
+// let headNavLinks = document.querySelectorAll('.nav-link');
+
+// headNavLinks.addEventListener("mouseenter", function (event) {
+//     event.target.style.color = "red";
+//     setTimeout(function() {
+//         event.target.style.color = "";
+//     }, 500);
+// }, false);
+
+// headNavLinks.addEventListener("mouseover", function (event) {
+//     event.target.style.color = "hotpink";
+//     setTimeout(function() {
+//         event.target.style.color = "";
+//     }, 500);
+// }, false);
+
+
+
+let headNavLinks = document.querySelectorAll("nav");
+
+headNavLinks.addEventListener("mouseover", function( event ) {
+  event.target.style.color = "red";
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+
+// headNavLinks.addEventListener("mouseover", function( event ) {
+//   event.target.style.color = "orange";
+//   setTimeout(function() {
+//     event.target.style.color = "";
+//   }, 500);
+// }, false);
+
