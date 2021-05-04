8532e6700fc8dbbee2bc6c37ca01a1ced758afc7
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-05-04 16:48:52 -0700

    completed one event listener: mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// 1. mouseover //
+
+let funBusHeading = document.querySelector(".logo-heading");
+
+funBusHeading.addEventListener("mouseover", function (event) {
+    event.target.style.color = "hotpink";
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 500);
+}, false);
+
+// 2. 
