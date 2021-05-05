d0cc62fab17bbd69f7ba05108b2da3cc1bc4ca14
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-05-04 17:10:57 -0700

    completed third event listener: double click

--- js/index.js
+++ js/index.js
@@ -21,3 +21,15 @@ allTextContent.addEventListener("keydown", function (event) {
         event.target.style.color = "";
     }, 500)
 }, false)
+
+// 3. select //
+
+let funBusTitle = document.querySelector('.main-navigation');
+
+funBusTitle.addEventListener('dblclick', function (event) {
+  event.target.style.background = "green";
+  setTimeout(function() {
+      event.target.style.background = "";
+  }, 500)
+
+}, false);
