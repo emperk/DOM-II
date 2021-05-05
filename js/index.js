93e42c33c3248e816907f574769fb5f8143450ab
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-05-04 17:37:52 -0700

    completed fifth even listener: click

--- js/index.js
+++ js/index.js
@@ -13,23 +13,50 @@ funBusHeading.addEventListener("mouseover", function (event) {
 
 // 2. keydown // 
 
-let allTextContent = document.querySelector("body");
+let bodyTextContent = document.querySelector("body");
 
-allTextContent.addEventListener("keydown", function (event) {
+bodyTextContent.addEventListener("keydown", function (event) {
     event.target.style.color = "orange";
     setTimeout(function() {
         event.target.style.color = "";
     }, 500)
 }, false)
 
-// 3. select //
+// 3. double click //
 
-let funBusTitle = document.querySelector('.main-navigation');
+let mainNavHeader = document.querySelector('.main-navigation');
 
-funBusTitle.addEventListener('dblclick', function (event) {
+mainNavHeader.addEventListener('dblclick', function (event) {
   event.target.style.background = "green";
   setTimeout(function() {
       event.target.style.background = "";
   }, 500)
 
 }, false);
+
+// 4. select //
+
+// function destinationSelection(event) {
+//   const destination = document.getElementsByClassName(".content-destination");
+//   const selection = event.target.style.background = "purple";
+//   setTimeout
+// }
+
+// destinationDiv.addEventListener('select', function (event) {
+//     event.target.style.background = "purple";
+//     setTimeout(function() {
+//         event.target.style.background = "";
+//     }, 500)
+// }, false);
+// let destinationDiv = document.querySelector('.content-destination');
+
+// 5. click //
+
+bodyTextContent.addEventListener("click", function (event) {
+  event.target.style.color = "purple";
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500)
+}, false);
+
+
